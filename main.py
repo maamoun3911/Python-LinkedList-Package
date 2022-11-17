ffbd379baf11931d65272f94fe7b42610ee4b8cf
Maamoun Haj Najeeb <87011464+maamoun3911@users.noreply.github.com>
2022-11-17 16:57:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 class Node:
-    def __init__(self, val) -> None:
+    def __init__(self, val=0) -> None:
         self.val = val
         self.next = None
 
@@ -9,7 +9,7 @@ class LList:
     def __init__(self) -> None:
         self.head = None
     
-    # insert function group
+    # insert functions group
     def append(self, val) -> None:
         if not self.head:
             self.head = Node(val)
@@ -29,17 +29,15 @@ class LList:
             pre = self.head
             while pre:
                 if pre.val == previous:
-                    newNode.next = pre.next
-                    pre.next = newNode
+                    pre.next = self.__swapping2Dimensions(newNode, pre.next)
                     break
                 pre = pre.next 
                 counting += 1
-            if counting == self.length():
+            if counting == self.__linkedListlength():
                 print("Sorry, but the key you entered isn't availabel")
     def insert_at_the_begging(self, val) -> None:
         newNode = Node(val)
-        newNode.next = self.head
-        self.head = newNode
+        self.head = self.__swapping2Dimensions(newNode, self.head)
     def sorted_insertion(self, val) -> str:
         newNode = Node(val)
         if not self.head: self.head = self.__swapping2Dimensions(newNode, self.head)
@@ -55,22 +53,61 @@ class LList:
                 pre.next = self.__swapping2Dimensions(newNode, curr)
                 return
             pre, curr = pre.next, curr.next
-
-    # helper methods
     
-    # swapping functions
-    def __swapping2Dimensions(self, newNode: Node, nextNode: Node) -> Optional[Node]:
-        newNode.next = nextNode
-        return newNode
+    # Delete functions group
+    def delete_from_the_begging(self):
+        if not self.head:
+            print("Empty Linked List")
+        self.head = self.head.next
+    def delete_from_the_end(self):
+        if not self.head:
+            print("Empty Linked List")
+        pre, curr = self.head, self.head.next
+        while curr.next:
+            curr, pre = curr.next, pre.next
+        pre.next = curr.next
+    def delete_from_the_middle(self, data):
+        if not self.head:
+            print("Empty Linked List")
+        elif self.head.val == data:
+            self.head = self.head.next
+        pre, curr = self.head, self.head.next
+        while curr:
+            if curr.val == data:
+                pre.next = curr.next
+                break
+            curr, pre = curr.next, pre.next
+    def delete(self):
+        self.head = None
+
+    # Update functions group
+    def update_first_element(self, data) -> None:
+        if not self.head: print("Empty Linked List")
+        elif self.head:
+            self.head.val = data
+    
+    def update_last_element(self, data) -> None:
+        if not self.head: print("Empty Linked List")
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        curr.val = data
+    
+    def update_a_middle_element(self, element, data) -> None:
+        if not self.head: print("Empty Linked List")
+        elif self.head.val == element:
+            self.head.val = data
+            return
+        curr = self.head
+        while curr:
+            if curr.val == element:
+                curr.val = data
+                return
+            curr = curr.next
 
     # Length
     def length(self) -> int:
-        lengthCounter: int = 0 
-        curr = self.head
-        while curr:
-            lengthCounter += 1
-            curr = curr.next
-        return lengthCounter
+        print(self.__linkedListlength())
     
     # Overviw
     def overview(self):
@@ -82,12 +119,35 @@ class LList:
             curr = curr.next
             if curr:
                 print(" -> ", end=" ")
+            else:
+                print()
+                return
+    
+    # View element
+    def view_element(self, data):
+        if not self.head:
+            print("Empty Linked List")
+        counter: int = 0
+        curr = self.head
+        while curr:
+            if curr.val == data:
+                print(f"type: {type(curr.val)}, next element: {curr.next}, element order: {counter}")
+                print(f"The element: {curr.val}")
+                break
+            curr = curr.next
+            counter += 1
 
-LinkedList = LList()
-LinkedList.append(0)
-LinkedList.insert_between_nodes(0, 1)
-LinkedList.append(3)
-LinkedList.insert_at_the_begging(-1)
-LinkedList.insert_at_the_end(9)
-LinkedList.sorted_insertion(-2)
-LinkedList.overview()
+    # helper methods
+    
+    # swapping functions
+    def __swapping2Dimensions(self, newNode: Node, nextNode: Node) -> Optional[Node]:
+        newNode.next = nextNode
+        return newNode
+    
+    def __linkedListlength(self) -> tuple[int, Node]:
+        lengthCounter: int = 0
+        curr = self.head
+        while curr:
+            lengthCounter += 1
+            curr = curr.next
+        return lengthCounter
